e96f474d93e0b3d21b09f81a5790070d407e750d
Wang Zuo <wzuoadjusted@gmail.com>
2018-06-04 15:36:34 +0800

    fix solc warning

--- __fixtures__/StandardToken.sol
+++ __fixtures__/StandardToken.sol
@@ -3,5 +3,7 @@ pragma solidity ^0.4.23;
 import './Token.sol';
 
 contract StandardToken is Token {
-  function transfer(address _to, uint256 _value) returns (bool success) {}
+  uint256 public totalSupply;
+
+  function transfer(address _to, uint256 _value) public returns (bool success);
 }
--- __fixtures__/Token.sol
+++ __fixtures__/Token.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.4.23;
 import './SafeMath.sol';
 
 contract Token {
-  function balanceOf(address _owner) constant returns (uint256 balance) {}
+  function balanceOf(address _owner) public view returns (uint256 balance);
 }
