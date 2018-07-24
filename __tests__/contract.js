8cc209c3c9e0fa10c07951515d8688b6bbd4c6ad
Wang Zuo <wzuoadjusted@gmail.com>
2018-07-24 16:42:00 +0800

    test SimpleStorage contract

--- __tests__/contract.js
+++ __tests__/contract.js
@@ -3,7 +3,14 @@ const solc = require('../src/solc');
 const { sleep, encodeFunctionSignature } = require('../src/utils');
 
 const client = new Client('http://localhost:8545');
-const contractSource = `pragma solidity ^0.4.23;
+let accounts = [];
+
+beforeAll(async () => {
+  accounts = await client.call('eth_accounts');
+});
+
+test('HelloWorld contract', async () => {
+  const source = `pragma solidity ^0.4.23;
 
 contract HelloWorld {
   function Hi() pure public returns(string) {
@@ -11,10 +18,8 @@ contract HelloWorld {
   }
 }`;
 
-test('HelloWorld contract', async () => {
-  const accounts = await client.call('eth_accounts');
   const account0 = accounts[0];
-  const { HelloWorld: contract } = await solc(contractSource, ['abi', 'bin']);
+  const { HelloWorld: contract } = await solc(source, ['abi', 'bin']);
 
   const tx = await client.call('eth_sendTransaction', [
     {
@@ -56,4 +61,32 @@ test('HelloWorld contract', async () => {
   const receipt1 = await client.call('eth_getTransactionReceipt', [tx1]);
   expect(tx1).not.toBeNull();
   expect(receipt1.from).toEqual(account0);
+});
+
+test('SimpleStorage contract', async () => {
+  const source = `pragma solidity ^0.4.0;
+
+contract SimpleStorage {
+  uint storedData;
+
+  function set(uint x) public {
+    storedData = x;
+  }
+
+  function get() public view returns (uint) {
+    return storedData;
+  }
+}`;
+
+  const account0 = accounts[0];
+  const { SimpleStorage: contract } = await solc(source, ['abi', 'bin']);
+  const tx = await client.call('eth_sendTransaction', [
+    {
+      from: account0,
+      data: '0x' + contract.bin,
+      gas: '0x' + Number(1000000).toString(16)
+    }
+  ]);
+
+  expect(tx).not.toBeNull();
 });
